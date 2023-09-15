a7ad4b9ff5824dc02e9efd09588cb23d695f6d52
Riteshgtm12@ <Riteshgtm12@gmail.com>
2023-09-15 14:09:18 +0530

    deploy error 3

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { SnackbarProvider } from 'notistack'       
 import { UserContextProvider } from './context/UserContext.jsx' 
-import { StyledEngineProvider } from '@mui/material/styles';   
+import { StyledEngineProvider } from '@mui/material/styles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
